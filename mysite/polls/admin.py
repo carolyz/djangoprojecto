a8e33e8198958bce4f630f45965996438467936d
carolyz <13carol.y.z@gmail.com>
2016-04-28 01:41:04 -0400

    added index.html, 404 pages

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question #import Question objs so it has admin interface
+admin.site.register(Question) #register Question so it's on the admin index page
